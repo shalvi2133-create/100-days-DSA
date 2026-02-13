dd23e0d9573689dec9e1fa7551aee2c774d6bbe1
shalvi2133-create <shalvi2133@gmail.com>
2026-02-13 21:39:03 +0530

    Add files via upload

---
+++ ques12.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    // Read matrix elements
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check if matrix is square
+    if(m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    // Check symmetry
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+
+    return 0;
+}
